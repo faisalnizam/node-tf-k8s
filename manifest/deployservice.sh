a3eda36bb33ef9052385160c43e2df8469e689a5
Faisal Nizam <faisalnizam@FaisalNizam-MacBook-Pro.local>
2021-12-25 12:11:34 +0400

    jenkins

--- manifest/deployservice.sh
+++ manifest/deployservice.sh
@@ -4,3 +4,14 @@ echo "Deploy Service on Your Cluster"
 echo "Create Namespace to deploy our services" 
 
 kubectl create namespace nodeapp
+
+kubectl create namespace jenkins
+
+# Install jenkins 
+helm repo add jenkinsci https://charts.jenkins.io
+helm repo update
+
+chart=jenkinsci/jenkins
+helm install jenkins -n jenkins -f jenkins/jk-override-values.yaml $chart
+
+
