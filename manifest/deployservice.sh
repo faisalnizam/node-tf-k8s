281dc8f0655a3efcdee6d7b00ff376190d0e65a9
Faisal Nizam <faisalnizam@FaisalNizam-MacBook-Pro.local>
2021-12-26 16:21:26 +0400

    issue added for creation certs

--- manifest/deployservice.sh
+++ manifest/deployservice.sh
@@ -15,3 +15,18 @@ chart=jenkinsci/jenkins
 helm install jenkins -n jenkins -f jenkins/jk-override-values.yaml $chart
 
 
+# Install Cert Manager 
+
+helm repo add jetstack https://charts.jetstack.io
+helm repo update
+
+
+helm install cert-manager jetstack/cert-manager --namespace cert-manager --create-namespace --version v1.5.3 --set installCRDs=true
+kubectl apply -f https://github.com/jetstack/cert-manager/releases/download/v1.5.3/cert-manager.crds.yaml
+
+
+curl -L -o kubectl-cert-manager.tar.gz https://github.com/jetstack/cert-manager/releases/latest/download/kubectl-cert_manager-linux-amd64.tar.gz
+tar xzf kubectl-cert-manager.tar.gz
+sudo mv kubectl-cert_manager /usr//bin
+
+
